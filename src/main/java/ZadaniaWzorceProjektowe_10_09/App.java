7658a2ea34c50f8f9fc079a83420dd801d48224c
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-09-10 12:02:15 +0200

    first commit

---
+++ src/main/java/ZadaniaWzorceProjektowe_10_09/App.java
@@ -0,0 +1,13 @@
+package ZadaniaWzorceProjektowe_10_09;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
