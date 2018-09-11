f27bc274a08d3b7b041894d168fc642e4d72b0a6
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-09-11 23:05:43 +0200

    Adding Zadanie3

--- src/main/java/ZadaniaWzorceProjektowe_10_09/App.java
+++ src/main/java/ZadaniaWzorceProjektowe_10_09/App.java
@@ -2,6 +2,7 @@ package ZadaniaWzorceProjektowe_10_09;
 
 import ZadaniaWzorceProjektowe_10_09.Zadanie1.*;
 import ZadaniaWzorceProjektowe_10_09.Zadanie2.*;
+import ZadaniaWzorceProjektowe_10_09.Zadanie3.*;
 
 public class App {
     public static void main(String[] args) {
@@ -43,5 +44,25 @@ public class App {
 
         System.out.println("________________________________________________________________");
 
+        // Zadanie3
+        System.out.println();
+        Employee topManager = new CompositeEmployee("John Smith", 10000); //root
+        Employee manager1 = new CompositeEmployee("Jack Star", 5000);
+        Employee manager2 = new CompositeEmployee("Eve Black", 5000);
+        topManager.addChild(manager1);
+        topManager.addChild(manager2);
+        Employee developer1 = new CompositeEmployee("Rick Geek", 2500);
+        Employee developer2 = new CompositeEmployee("Ben Smoke", 2500);
+        Employee developer3 = new CompositeEmployee("Bill Smile", 2500);
+        manager1.addChild(developer1);
+        manager1.addChild(developer2);
+        manager1.addChild(developer3);
+        Employee developer4 = new CompositeEmployee("Pete Rock", 2500);
+        Employee developer5 = new CompositeEmployee("Meg Song", 2500);
+        Employee developer6 = new CompositeEmployee("Liana Xi", 2500);
+        manager2.addChild(developer4);
+        manager2.addChild(developer5);
+        manager2.addChild(developer6);
+        topManager.showMe();
     }
 }
